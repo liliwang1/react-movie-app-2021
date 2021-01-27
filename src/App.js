6ae4f5222cd100d9ec40eb6cdf5e6a8b560b4654
liliwang <monsi0510@hotmail.com>
2021-01-26 23:28:33 -0600

    Get movies data using es6 object destructuring

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    console.log(movies.data.data.movies);
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    console.log(movies);
   };
 
   componentDidMount() {
