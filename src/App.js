6e436aeff1f72b677b79dee05bb0bd7aaee08118
liliwang <monsi0510@hotmail.com>
2021-01-30 23:54:47 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
 
 function App() {
-  return <h1>hi</h1>;
+  return (
+    <HashRouter>
+      <Route path='/' component={Home} exact={true} />
+      <Route path='/about' component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
