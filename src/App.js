9489a7ba9c93589db888ad85235065729df2a40e
liliwang <monsi0510@hotmail.com>
2021-01-30 23:31:20 -0600

    Add react-router-dom and organize folder structure

--- src/App.js
+++ src/App.js
@@ -1,60 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-
-    console.log(movies);
-
-    this.setState({
-      isLoading: false,
-      movies,
-    });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className='container'>
-        {isLoading ? (
-          <div className='loader'>
-            <span className='loader-text'>Loading...</span>
-          </div>
-        ) : (
-          <div className='movies'>
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-                year={movie.year}
-                rating={movie.rating}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return <h1>hi</h1>;
 }
 
 export default App;
